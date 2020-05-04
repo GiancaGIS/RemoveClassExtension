de576e129e5016de4cd3a6df0895344fef11e454
GiancaGIS <63131168+GiancaGIS@users.noreply.github.com>
2020-05-04 08:32:15 +0200

    Insert new ArcObjects Code / Updated existing

--- RemoveClassExtension.cs
+++ RemoveClassExtension.cs
@@ -1,13 +1,15 @@
 ﻿namespace StudioAT.ArcGIS.ArcCatalog.AddIn.RemoveClassExtension
 {
-    using System;
-    using System.Runtime.InteropServices;
-    using System.Windows.Forms;
     using ESRI.ArcGIS.Carto;
     using ESRI.ArcGIS.Catalog;
     using ESRI.ArcGIS.CatalogUI;
     using ESRI.ArcGIS.esriSystem;
+    using ESRI.ArcGIS.Framework;
     using ESRI.ArcGIS.Geodatabase;
+    using StudioAT.ArcGIS.ArcCatalog.AddIn.RemoveClassExtension.Generic;
+    using System;
+    using System.Runtime.InteropServices;
+    using System.Windows.Forms;
 
     public class RemoveClassExtension : ESRI.ArcGIS.Desktop.AddIns.Button
     {
@@ -15,129 +17,72 @@
 
         public RemoveClassExtension()
         {
-            this.pGxApp  = ArcCatalog.Application as IGxApplication;
-           
+            this.pGxApp = ArcCatalog.Application as IGxApplication;
         }
 
         protected override void OnClick()
         {
             try
             {
-                if (this.pGxApp.Selection.Count != 1)
+                IGxSelection gxSelection = this.pGxApp.Selection; 
+                
+                if (gxSelection.Count < 1)
                 {
                     return;
                 }
 
+                // Inizializzo le variabili per la progress bar...
+                ITrackCancel trkCancel = null;
+                IProgressDialogFactory prgFact = new ProgressDialogFactoryClass();
+                IStepProgressor stepProgressor = null;
+                IProgressDialog2 progressDialog = null;
 
-                IGxObject pGxObject = this.pGxApp.SelectedObject;
+                int intNumberFcConverted = 0;
+                int cont = gxSelection.Count;
+                IEnumGxObject enumGxObject = gxSelection.SelectedObjects;
 
+                stepProgressor = prgFact.Create(trkCancel, 0);
+                progressDialog = stepProgressor as IProgressDialog2;
+                progressDialog.Description = "Removing extensions...";
+                progressDialog.Title = "Removing extensions...";
+                progressDialog.Animation = esriProgressAnimationTypes.esriProgressSpiral;
+                progressDialog.ShowDialog();
 
-                if (!(pGxObject is IGxDataset))
+                stepProgressor.MinRange = 0;
+                stepProgressor.MaxRange = cont;
+                stepProgressor.StepValue = 1;
+                stepProgressor.Show();
+
+                IGxObject pGxObject = enumGxObject.Next();
+
+                while (pGxObject != null)
                 {
-                    return;
-                }
-
-                IGxDataset pGxDataset = pGxObject as IGxDataset;
-                if (pGxDataset == null)
-                {
-                    return;
-                }
-
-                if (((pGxObject as IGxDataset).Type) != esriDatasetType.esriDTFeatureClass)
-                {
-                    return;
-                }
-
-                try
-                {
-                    IDataset a = pGxDataset.Dataset;
-                }
-                catch (COMException COMex)
-                {
-                    if (COMex.ErrorCode == -2147467259)
+                    if (!(pGxObject is IGxDataset))
                     {
-                        if (MessageBox.Show("Not found component register so I don't see the UID: do I have to remove class extension? Are you sure?", "Remove class extension", MessageBoxButtons.YesNo) == DialogResult.Yes)
-                        {
-                            try
-                            {
-                                IGxObject gxObject = pGxObject.Parent;
-                                if (!(gxObject is IGxDatabase2))
-                                {
-                                    gxObject = gxObject.Parent;
-                                }
-
-                                IFeatureWorkspaceSchemaEdit featureWorkspaceSchemaEdit = ((gxObject as IGxDatabase2).Workspace) as IFeatureWorkspaceSchemaEdit;
-                                featureWorkspaceSchemaEdit.AlterClassExtensionCLSID(pGxDataset.DatasetName.Name, null, null);
-
-                                IObjectClassDescription featureClassDescription = new FeatureClassDescriptionClass();
-                                featureWorkspaceSchemaEdit.AlterInstanceCLSID(pGxDataset.DatasetName.Name, featureClassDescription.InstanceCLSID);
-
-                                MessageBox.Show("Class extension removed: success! Restart ArcCatalog!", "Remove Class Extension", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                            }
-                            catch(Exception ex)
-                            {
-                                MessageBox.Show("Error " + ex.Message);
-                                return;
-                            }
-                        }
-                    }
-                    else
-                    {
-                        MessageBox.Show("Error " + COMex.ErrorCode.ToString() + ": " + COMex.Message);
-                    }
-
-                    return;
-                }
-                catch (System.Exception ex)
-                {
-                    MessageBox.Show("Error: " + ex.Message);
-                    return;
-                }
-
-
-                if (!(pGxDataset.Dataset is IClass))
-                {
-                    return;
-                }
-
-                IClass pClass = pGxDataset.Dataset as IClass;
-
-                if (pClass.EXTCLSID == null)
-                {
-                    MessageBox.Show("No class extension!");
-                    return;
-                }
-                else
-                {
-
-                    IObjectClassDescription ocDescription = new AnnotationFeatureClassDescriptionClass();
-                    if ((pClass.EXTCLSID.Value.ToString() == ocDescription.ClassExtensionCLSID.Value.ToString()) && (pClass.EXTCLSID.SubType == ocDescription.ClassExtensionCLSID.SubType))
-                    {
-                        MessageBox.Show("Class extension well-know: I don't remove it (annotation)!", "Remove Class Extension", MessageBoxButtons.OK, MessageBoxIcon.Warning);
                         return;
                     }
 
-                    ocDescription = new DimensionClassDescriptionClass();
-                    if ((pClass.EXTCLSID.Value.ToString() == ocDescription.ClassExtensionCLSID.Value.ToString()) && (pClass.EXTCLSID.SubType == ocDescription.ClassExtensionCLSID.SubType))
+                    IGxDataset pGxDataset = pGxObject as IGxDataset;
+                    if (pGxDataset == null)
                     {
-                        MessageBox.Show("Class extension well-know: I don't remove it (dimension)!", "Remove Class Extension", MessageBoxButtons.OK, MessageBoxIcon.Warning);
                         return;
                     }
 
+                    //if (((pGxObject as IGxDataset).Type) != esriDatasetType.esriDTFeatureClass)
+                    //{
+                    //    return;
+                    //}
 
-
-                    if (MessageBox.Show(string.Format("Class extension: {0}: do I have to remove class extension?", pClass.EXTCLSID.Value.ToString()), "Remove Class Extension", MessageBoxButtons.YesNo) == DialogResult.Yes)
-                    {
-                        if (this.RemoveClsExt(pClass))
-                        {
-                            MessageBox.Show("Class extension removed: success!", "Remove Class Extension", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                        }
-                        else
-                        {
-                            MessageBox.Show("Class extension removed: error! I have problem with exclusive schema lock ", "Remove Class Extension", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                        }
-                    }
+                    this.engine(pGxDataset, pGxObject, ref intNumberFcConverted);
+                    pGxObject = enumGxObject.Next();
+                    stepProgressor.Step();
                 }
+
+                stepProgressor.Message = "End";
+                stepProgressor.Hide();
+                progressDialog.HideDialog();
+
+                MessageBox.Show($@"Extension removed for {intNumberFcConverted} objects!", "Attention", MessageBoxButtons.OK, MessageBoxIcon.Information);
 
             }
             catch (Exception ex)
@@ -147,18 +92,112 @@
 
         }
 
+        private void engine(IGxDataset pGxDataset, IGxObject pGxObject, ref int i)
+        {
+            try
+            {
+                IDataset dataset = pGxDataset.Dataset;
+            }
+            catch (COMException COMex)
+            {
+                if (COMex.ErrorCode == -2147467259)
+                {
+                    if (MessageBox.Show("Not found component register so I don't see the UID: do I have to remove class extension? Are you sure?", "Remove class extension", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                    {
+                        try
+                        {
+                            IGxObject gxObject = pGxObject.Parent;
+                            if (!(gxObject is IGxDatabase2))
+                            {
+                                gxObject = gxObject.Parent;
+                            }
+
+                            IFeatureWorkspaceSchemaEdit featureWorkspaceSchemaEdit = ((gxObject as IGxDatabase2).Workspace) as IFeatureWorkspaceSchemaEdit;
+                            featureWorkspaceSchemaEdit.AlterClassExtensionCLSID(pGxDataset.DatasetName.Name, null, null);
+
+                            IObjectClassDescription featureClassDescription = new FeatureClassDescriptionClass();
+                            featureWorkspaceSchemaEdit.AlterInstanceCLSID(pGxDataset.DatasetName.Name, featureClassDescription.InstanceCLSID);
+
+                            MessageBox.Show("Class extension removed: success! Restart ArcCatalog!", "Remove Class Extension", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show("Error " + ex.Message);
+                            return;
+                        }
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Error " + COMex.ErrorCode.ToString() + ": " + COMex.Message);
+                }
+
+                return;
+            }
+            catch (System.Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+                return;
+            }
+
+
+            if (!(pGxDataset.Dataset is IClass))
+            {
+                return;
+            }
+
+            IClass pClass = pGxDataset.Dataset as IClass;
+
+            if (pClass.EXTCLSID == null)
+            {
+                //MessageBox.Show("No class extension!");
+                return;
+            }
+            else
+            {
+
+                IObjectClassDescription ocDescription = new AnnotationFeatureClassDescriptionClass();
+                if ((pClass.EXTCLSID.Value.ToString() == ocDescription.ClassExtensionCLSID.Value.ToString()) && (pClass.EXTCLSID.SubType == ocDescription.ClassExtensionCLSID.SubType))
+                {
+                    MessageBox.Show("Class extension well-know: I don't remove it (annotation)!", "Remove Class Extension", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                ocDescription = new DimensionClassDescriptionClass();
+                if ((pClass.EXTCLSID.Value.ToString() == ocDescription.ClassExtensionCLSID.Value.ToString()) && (pClass.EXTCLSID.SubType == ocDescription.ClassExtensionCLSID.SubType))
+                {
+                    MessageBox.Show("Class extension well-know: I don't remove it (dimension)!", "Remove Class Extension", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                if (MessageBox.Show(string.Format("Class extension: {0}: do I have to remove class extension?", pClass.EXTCLSID.Value.ToString()), "Remove Class Extension", MessageBoxButtons.YesNo) 
+                    == DialogResult.Yes)
+                {
+                    if (clsGenericStudioAT.RemoveClsExt(pClass))
+                    {
+                        i++;
+                        //MessageBox.Show("Class extension removed: success!", "Remove Class Extension", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Class extension removed: error! I have problem with exclusive schema lock ", "Remove Class Extension", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+        }
+
         protected override void OnUpdate()
         {
-            if (this.pGxApp.Selection.Count != 1)
+            if (this.pGxApp.Selection.Count < 0)
             {
                 this.Enabled = false;
                 return;
             }
-
-            IGxObject pGxObject = this.pGxApp.SelectedObject;
-
-            this.Enabled = ((pGxObject as IGxDataset) != null) && ((pGxObject as IGxDataset).Type == esriDatasetType.esriDTFeatureClass);
-
+            else
+            {
+                this.Enabled = true;
+                return;
+            }
         }
 
         private bool FeatureClassWellKnown(UID uid)
@@ -178,37 +217,6 @@
             return false;
         }
 
-        private bool RemoveClsExt(IClass classExtension)
-        {
-            ISchemaLock schemaLock = (ISchemaLock)classExtension;
-            bool result = false;
-            try
-            {
-                // Attempt to get an exclusive schema lock.
-                schemaLock.ChangeSchemaLock(esriSchemaLock.esriExclusiveSchemaLock);
-
-                // Cast the object class to the IClassSchemaEdit2 interface.
-                IClassSchemaEdit2 classSchemaEdit = (IClassSchemaEdit2)classExtension;
-
-                // Clear the class extension.
-                classSchemaEdit.AlterClassExtensionCLSID(null, null);
-
-                IObjectClassDescription featureClassDescription = new FeatureClassDescriptionClass();
-                classSchemaEdit.AlterInstanceCLSID(featureClassDescription.InstanceCLSID);
-
-                result = true;
-            }
-            catch
-            {
-                result = false;
-            }
-            finally
-            {
-                schemaLock.ChangeSchemaLock(esriSchemaLock.esriSharedSchemaLock);
-            }
-
-            return result;
-        }
     }
 
     
---
+++ removeAllExt.cs
@@ -0,0 +1,105 @@
+﻿using ESRI.ArcGIS.Carto;
+using ESRI.ArcGIS.Catalog;
+using ESRI.ArcGIS.CatalogUI;
+using ESRI.ArcGIS.Geodatabase;
+using StudioAT.ArcGIS.ArcCatalog.AddIn.RemoveClassExtension.Generic;
+using System;
+using System.Windows.Forms;
+
+
+namespace StudioAT.ArcGIS.ArcCatalog.AddIn.RemoveClassExtension
+{
+    public class removeAllExt : ESRI.ArcGIS.Desktop.AddIns.Button
+    {
+        IGxApplication pGxApp = null;
+        IGxContentsView contentsView = null;
+
+        public removeAllExt()
+        {
+            this.pGxApp = ArcCatalog.Application as IGxApplication;
+            this.contentsView = this.pGxApp as IGxContentsView;
+        }
+
+        protected override void OnClick()
+        {
+            try
+            {
+                if (this.pGxApp.Selection.Count != 1)
+                {
+                    return;
+                }             
+
+                IGxObject pGxObject = this.pGxApp.SelectedObject;
+
+
+                if (!(pGxObject is IGxDataset))
+                {
+                    return;
+                }
+
+                IGxDataset pGxDataset = pGxObject as IGxDataset;
+
+                if (pGxDataset == null)
+                {
+                    return;
+                }
+
+                if (((pGxObject as IGxDataset).Type) != esriDatasetType.esriDTFeatureDataset)
+                {
+                    MessageBox.Show("Select a Feature Dataset!", "Attention", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    return;
+                }
+
+                IFeatureDataset fDataset = pGxDataset.Dataset as IFeatureDataset;
+                IWorkspace2 workspace = fDataset.Workspace as IWorkspace2;
+                IFeatureWorkspace featureWorkspace = workspace as IFeatureWorkspace;
+                
+                
+                DialogResult dialogResult =
+                MessageBox.Show($@"Remove custom extension for all Feature Classes in {fDataset.Name}", "Attention", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+                if (dialogResult == DialogResult.Yes)
+                {
+                    IEnumDataset enumDataset = fDataset.Subsets;
+                    IDataset dataset = enumDataset.Next();
+
+                    IObjectClassDescription ocDescriptionAnnotation = new AnnotationFeatureClassDescriptionClass();
+                    IObjectClassDescription ocDescriptionDimension = new DimensionClassDescriptionClass();
+
+                    while (dataset != null)
+                    {
+                        if (dataset is IClass)
+                        {
+                            IClass pClass = dataset as IClass;
+                            if (pClass.EXTCLSID != null)
+                            {
+                                if ((pClass.EXTCLSID.Value.ToString().ToLower() != ocDescriptionAnnotation.ClassExtensionCLSID.Value.ToString().ToLower())
+                                    && (pClass.EXTCLSID.SubType != ocDescriptionAnnotation.ClassExtensionCLSID.SubType)
+                                    && (pClass.EXTCLSID.Value.ToString().ToLower() != ocDescriptionDimension.ClassExtensionCLSID.Value.ToString().ToLower())
+                                    && (pClass.EXTCLSID.SubType != ocDescriptionDimension.ClassExtensionCLSID.SubType))
+                                {
+                                    clsGenericStudioAT.RemoveClsExt(pClass);
+                                }
+                            }
+                        }
+
+                        dataset = enumDataset.Next();
+                    }
+
+                    MessageBox.Show("All Extension removed!", "Ok", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+
+            }
+            catch (Exception errore)
+            {
+                MessageBox.Show(errore.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                throw;
+            }
+        }
+
+        protected override void OnUpdate()
+        {
+            this.Enabled = true;
+        }
+    }
+}
